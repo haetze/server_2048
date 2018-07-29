5b5f3766c4d88416f2f2d8fc74cd1447a4e4480a
Richard Stewing <richy.sting@gmail.com>
2018-07-29 16:20:12 +0200

    beginning connection handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate lib_2048;
 
 use std::net::TcpListener;
+use std::net::TcpStream;
 use std::env;
 use lib_2048::data;
 
@@ -15,4 +16,19 @@ fn main() {
         None    => default_port,
     };
     println!("{}", port_requested);
+    match TcpListener::bind("127.0.0.1:8080") {
+        Err(_) => println!("Port unavailable, restart with different port."),
+        Ok(listener) => {
+            match listener.accept() {
+                Ok((socket, _)) => handle_messages(socket),
+                Err(_)          => println!("Fail to accept connection, try again."),
+            }
+        },
+    }
+    
+}
+
+fn handle_messages(socket: TcpStream) {
+    
+    
 }
